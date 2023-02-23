b911065ffb889961d33a046bd0427f14317401bf
kameshwarasekar <kameshwarasekar@gmail.com>
2023-02-23 13:25:34 -0700

    add scipt for extracting dataset and database connection

---
+++ db.py
@@ -0,0 +1,138 @@
+
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+#def create_server_connection(host_name, user_name, user_password):
+#    connection = None
+#    try:
+#        connection = mysql.connector.connect(
+#                host = host_name,
+#                user = user_name,
+#                password = user_password
+#        )
+#        print("Successfully connected to DB")
+#    except Error as err:
+#        print(f"Error:'{err}'")
+#    return connection
+#
+#connection = create_server_connection('127.0.0.1', 'root', 'password')
+#
+#def create_database(connection, query):
+#    cursor = connection.cursor()
+#    try:
+#        cursor.execute(query)
+#        print('Database created successfully')
+#    except Error as err:
+#        print(f"Error '{err}'")
+ 
+
+
+#create_db_query = 'CREATE DATABASE houseOfTrivia'
+#create_database(connection, create_db_query)
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+                host = host_name,
+                user = user_name,
+                password = user_password,
+                database = db_name
+        )
+        print("Successfully connected to DB")
+    except Error as err:
+        print(f"Error:'{err}'")
+    return connection
+
+connection = create_db_connection('localhost', 'root', 'password', 'houseOfTrivia')
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+
+create_questions_table = '''
+CREATE TABLE questions (
+    question_id INT PRIMARY KEY,
+    question VARCHAR(2000) NOT NULL,
+    answer VARCHAR(100) NOT NULL,
+    option_1 VARCHAR(100) NOT NULL,
+    option_2 VARCHAR(100) NOT NULL,
+    option_3 VARCHAR(100) NOT NULL,
+    option_4 VARCHAR(100) NOT NULL
+);'''
+
+#execute_query(connection, create_questions_table)
+
+
+files = open('history.txt', 'r+')
+content = files.readlines()
+
+
+'''
+Writing multiple rows into sql database using python, create a list of 
+tuples and follow the example code below
+rows = [(1,7,3000), (1,8,3500), (1,9,3900)]
+values = ', '.join(map(str, rows))
+sql = "INSERT INTO ... VALUES {}".format(values)
+'''
+
+
+question_bank = [] #list of tuples
+question = ''
+answer = ''
+option_1 = ''
+option_2 = ''
+option_3 = ''
+option_4 = ''
+
+
+length = len(content)
+i = 0
+while i < length:
+    if len(content[i].strip()) == 0: 
+        i+=1
+        continue
+    if content[i][0] == '#':
+        if content[i][-2] not in ['?','.']:
+            while  i< length and len(content[i].strip()) != 0:
+                i+=1
+        else:
+            line = content[i:i+7]
+            print(line)
+            print('\n')
+            if len(line) != 7:
+                #print(len(line)+ ' is the length of the line')
+                break
+            if line[4][0]!='C':
+                i+=4
+                #print(line[5]+' is not C') 
+                continue
+            question_bank.append((i//7,line[0][3:-1],line[1][2:-1],line[2][2:-1],
+                line[3][2:-1],line[4][2:-1],line[5][2:-1]))
+            i+=6
+    i+=1
+#
+#length = len(content) #11179
+#for i in range(0, length, 7):  #Upper limit should be multiple of 7
+#    line = content[i:i+7]
+#    if len(line) == 7:
+#        if line[5][0]!='C':
+#            i-=3
+#            continue
+#        question_bank.append((i//7, line[1][3:-1], line[2][2:-1], line[3][2:-1],
+#                line[4][2:-1], line[5][2:-1], line[6][2:-1]))
+
+
+
+print(question_bank)
+
+
