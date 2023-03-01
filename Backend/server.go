314f1484dad5a89ebce02437b59211fc5e49f7b6
Apostolos <apostolos.scondriani@ucalgary.ca>
2023-03-01 09:58:39 -0700

    server.go v.0.2 Game Service Listener

--- Backend/server.go
+++ Backend/server.go
@@ -1,16 +1,18 @@
 //Author         : Apostolos Scondrianis
 //Created On     : 28-02-2023
 //Last Edited By : Apostolos Scondrianis
-//Last Edit On   : 28-02-2023
-//Filename       : proxy.go
-//Version        : 0.1
+//Last Edit On   : 01-03-2023
+//Filename       : server.go
+//Version        : 0.2
 
 package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -19,17 +21,68 @@ type connection struct {
 	port string;
 	con_type string;
 }
-var  PROXY = connection{"127.0.0.1", "9000", "tcp"}
-var SERVER = connection{"127.0.0.1", "8080", "tcp"}
+var PROXY = connection{"127.0.0.1", "9000", "tcp"}
+var GAME_SERVICE = connection{"127.0.0.1", "8080", "tcp"}
 
 func main() {
 	if(connectToProxy()) {
+		//Listen for Game Service Requests if you were successfully registered at the Proxy
 		fmt.Println("Successful registration to proxy.")
+		gameServiceTCPAddr, err := net.ResolveTCPAddr(GAME_SERVICE.con_type, GAME_SERVICE.host+":"+GAME_SERVICE.port)
+		if err != nil {
+			fmt.Printf("Unable to resolve IP")
+		}
+	
+		// Start TCP Listener for the server
+		listener, err := net.ListenTCP("tcp", gameServiceTCPAddr)
+		if err != nil {
+			fmt.Printf("Unable to start listener - %s", err)
+		} else {
+			fmt.Printf("Listening on %v:%v\n", GAME_SERVICE.host, GAME_SERVICE.port)
+		}
+		//close Listener
+		defer listener.Close()
+	
+		//Continuously Listen for connections
+		for {
+			conn, err:= listener.Accept()
+			if err != nil {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+			fmt.Printf("Incoming connection from : %s\n", conn.RemoteAddr().String())
+			go handleGameRequest(conn)
+		}
 	} else {
 		fmt.Println("Failed to register server at the proxy.")
 	}
 }
 
+func handleGameRequest(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if strings.Compare(string(buffer[:n]), "Game Room") == 0 {
+		//Game Connection Attempt.
+		conn.Write([]byte("Game Room ID"))
+		n, err = conn.Read(buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		roomID, err := strconv.Atoi(string(buffer[:n]))
+		if err != nil {
+			fmt.Println("Error during conversion")
+		} else {
+			fmt.Printf("Proxy wants us to handle Game Room with ID %d\n", roomID)
+		}
+	} else {
+		fmt.Println("Invalid Option Given by the proxy.")
+	}
+	// close conn
+	conn.Close()
+}
 
 //Returns true if the proxy accepts the connection.
 func connectToProxy() bool {
@@ -61,8 +114,8 @@ func connectToProxy() bool {
 	if strings.Compare(string(received[:n]), "Accepted") == 0 {
 		//If the proxy accepted us, send the address we will be serving at
 		fmt.Printf("Received message: %s.\n", string(received[:n]))
-		var serverAddr = SERVER.host+":"+SERVER.port
-		_, err = conn.Write([]byte(serverAddr))
+		var gameServiceAddress = GAME_SERVICE.host+":"+GAME_SERVICE.port
+		_, err = conn.Write([]byte(gameServiceAddress))
 		if(err != nil) {
 			fmt.Println("Write data failed:", err.Error())
 			os.Exit(1)
