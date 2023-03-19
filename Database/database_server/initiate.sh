f611103f3eaba7e3e0f314680fdfc41b9485da75
Long Tran <long-ht@outlook.com>
2023-03-19 00:23:00 -0600

    maybe working ?

--- Database/database_server/initiate.sh
+++ Database/database_server/initiate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-create_slave_stmt="GRANT REPLICATION SLAVE ON *.* TO mydb_user; FLUSH PRIVILEGES;"
+create_slave_stmt="GRANT REPLICATION SLAVE ON *.* TO mydb_user_$ID; FLUSH PRIVILEGES;"
+echo "$create_slave_stmt"
 mysql -u root -ppassword -e "$create_slave_stmt"
 
 if ping -c1 db"$REPLICA_ID" ; then
     echo "DB$REPLICA_ID is online. Setting db$ID as slave"
-    start_slave_stmt="CHANGE MASTER TO MASTER_HOST='db$REPLICA_ID',MASTER_PORT=3306,MASTER_USER='mydb_user',MASTER_PASSWORD='mydb_pwd',MASTER_AUTO_POSITION=1; START SLAVE;"
+    start_slave_stmt="RESET SLAVE; CHANGE MASTER TO MASTER_HOST='db$REPLICA_ID',MASTER_USER='mydb_user_$REPLICA_ID',MASTER_PASSWORD='mydb_pwd_$REPLICA_ID',MASTER_AUTO_POSITION=1; START SLAVE;"
+    echo "$start_slave_stmt"
     mysql -u root -ppassword -e "$start_slave_stmt"
 
 else
