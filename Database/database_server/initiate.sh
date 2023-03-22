95ccf41c7f629865702de9a305724ee0ea187988
Long Tran <long-ht@outlook.com>
2023-03-21 23:06:57 -0600

    finish database

--- Database/database_server/initiate.sh
+++ Database/database_server/initiate.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-create_slave_stmt="GRANT REPLICATION SLAVE ON *.* TO mydb_user_$ID; FLUSH PRIVILEGES;"
-echo "$create_slave_stmt"
-mysql -u root -ppassword -e "$create_slave_stmt"
-
 if ping -c1 db"$REPLICA_ID" ; then
     echo "DB$REPLICA_ID is online. Setting db$ID as slave"
-    start_slave_stmt="RESET SLAVE; CHANGE MASTER TO MASTER_HOST='db$REPLICA_ID',MASTER_USER='mydb_user_$REPLICA_ID',MASTER_PASSWORD='mydb_pwd_$REPLICA_ID',MASTER_AUTO_POSITION=1; START SLAVE;"
+    mysql --host="db$REPLICA_ID" -u root -ppassword -e "CREATE USER IF NOT EXISTS 'mydb_user_$REPLICA_ID'@'%' IDENTIFIED BY 'mydb_pwd_$REPLICA_ID';GRANT REPLICATION SLAVE ON *.* TO 'mydb_user_$REPLICA_ID'@'%'; FLUSH PRIVILEGES;"
+    mysql --host="db$REPLICA_ID" -u root -ppassword -e "RESET MASTER;FLUSH TABLES WITH READ LOCK;SHOW MASTER STATUS;"
+    mysqldump --host="db$REPLICA_ID" -u root -ppassword --all-databases --master-data=2 > /sqldump/dump.sql
+    mysql --host="db$REPLICA_ID" -u root -ppassword -e "UNLOCK TABLES;"
+    mysql -u root -ppassword -e "STOP SLAVE;"
+    mysql -u root -ppassword < /sqldump/dump.sql
+    CURRENT_LOG=$(mysql --host="db$REPLICA_ID" -u root -ppassword -e "show master status" -s | tail -n 1 | awk {'print $1'})
+    CURRENT_POS=$(mysql --host="db$REPLICA_ID" -u root -ppassword -e "show master status" -s | tail -n 1 | awk {'print $2'})
+    start_slave_stmt="RESET SLAVE;CHANGE MASTER TO MASTER_HOST='db$REPLICA_ID',MASTER_USER='mydb_user_$REPLICA_ID',MASTER_PASSWORD='mydb_pwd_$REPLICA_ID',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
     echo "$start_slave_stmt"
     mysql -u root -ppassword -e "$start_slave_stmt"
 
